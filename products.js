ca86a70fcc8a84653edda5d46c79b26ecadd62e5
johnpaultort <johnpaultort@csu.fullerton.edu>
2026-06-13 00:44:05 -0700

    I apologize, I started working on this 6/11/26. Structure of the website or designs is still not clear. Ccentreall is set to release in about 1-2 months, during this time plan to work with the brand owner and get all logic running and give them a result they are pleasant with.

---
+++ products.js
@@ -0,0 +1,24 @@
+// ============================================================
+// products.js
+//  name: name of item being sold
+//  price: your actual price int
+//  image: path to image
+//  description: Vintage T or message from oscar
+//   stripeLink: TBD
+//   tag: "blank", "limited"
+//   soldOut: set to true to show "Sold Out" on the card
+// ============================================================
+
+const products = [
+  {
+    id: 1,
+    name: "INSIDEOUT",                   
+    price: 45,                          
+    image: "images/insideout.png",
+    description: "Vintage Distressed T, with a catch its inside out",
+    stripeLink: "#",
+    tag: "limited",
+    soldOut: false,
+    sizes: ["1", "2", "3"]
+  }
+]
